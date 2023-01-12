20e2600199559c238e11130346a05f0fc062057b
Yelk11 <neighbour.matthew@gmail.com>
2023-01-11 20:28:30 -0500

    added expressions ast node

--- include/ast.h
+++ include/ast.h
@@ -5,7 +5,6 @@
 
 typedef struct ast_T
 {
-
     struct decl
     {
         char *name;
@@ -13,7 +12,7 @@ typedef struct ast_T
         struct expr *value;
         struct stmt *code;
         struct decl *next;
-    }decl;
+    } decl;
     struct stmt
     {
         enum stmt_t
@@ -25,7 +24,7 @@ typedef struct ast_T
             STMT_PRINT,
             STMT_RETURN,
             STMT_BLOCK
-        }stmt_t;
+        } stmt_t;
         enum stmt_t kind;
         struct decl *decl;
         struct expr *init_expr;
@@ -34,10 +33,27 @@ typedef struct ast_T
         struct stmt *body;
         struct stmt *else_body;
         struct stmt *next;
-    }stmt;
-    struct exp
+    } stmt;
+    struct expr
     {
-    }exp;
+        enum expr_t
+        {
+            EXPR_ADD,
+            EXPR_SUB,
+            EXPR_MUL,
+            EXPR_DIV,
+            EXPR_NAME,
+            EXPR_INTEGER_LITERAL,
+            EXPR_STRING_LITERAL
+        } expr_t;
+        enum expr_t kind;
+        struct expr* left;
+        struct expr* right;
+        const char *name;
+        int integer_value;
+        const char* string_literal;
+    }expr;
+
 } ast_T;
 
 #endif
